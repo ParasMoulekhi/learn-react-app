867c017470ce7b81810c926d259cf6aebee40b43
Paras <parasmoulekhi11@gmail.com>
2023-12-06 07:40:42 +0530

    Initial commit

---
+++ React-Component/Projects/7-social-media/src/store/post-list-store.jsx
@@ -0,0 +1,62 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currentPostList, action) => {
+  let newPostList = currentPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currentPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  }
+
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = () => {};
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacations", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Pass hogay Bhai",
+    body: "4 saal ki masti k baad bhi ho gaye hain pass. Hard to believe",
+    reactions: 15,
+    userId: "user-15",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
+
+export default PostListProvider;
